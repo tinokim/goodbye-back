421f1ccc163037de1b286709ad6b82ed41a3e27c
tinokim <gksmfwlsgh@nate.com>
2023-10-09 02:39:16 +0900

    goodbye-back

---
+++ src/main/java/com/goodbyeback/goodbyeback/GoodbyeBackApplication.java
@@ -0,0 +1,13 @@
+package com.goodbyeback.goodbyeback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodbyeBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodbyeBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/goodbyeback/goodbyeback/GoodbyeBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goodbyeback.goodbyeback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodbyeBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
